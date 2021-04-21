0dca78f8a14ca30c36037f96625229b781f58f46
Cleberson Saller <cleberson600@gmail.com>
2021-04-21 18:34:49 -0300

    removed unnecessary variable

--- utils/bot/guild.js
+++ utils/bot/guild.js
@@ -2,8 +2,7 @@ const client = require('./client')
 
 async function fetchGuild (serverID) {
   try {
-    const Guild = await client.guilds.fetch(serverID)
-    return Guild
+    return await client.guilds.fetch(serverID)
   } catch (err) {
     return new Error(err.message)
   }
